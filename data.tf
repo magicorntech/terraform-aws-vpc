0a049e209dbb4981bbbec1eb4fdcbee3db1ede0a
Umur Coskuncan | Magicorn <umur@magicorn.co>
2023-05-31 00:23:11 +0300

    Bool fix for non private networks

--- data.tf
+++ data.tf
@@ -4,6 +4,6 @@ locals {
   nat_count = (
     (length(var.pvt_sub_count) > 0 && var.single_az_nat == true) ? 1 : false ||
     (length(var.pvt_sub_count) > 0 && var.single_az_nat == false) ? length(var.pbl_sub_count) : false ||
-    (length(var.pvt_sub_count) == 0) ? 0 : false
+    (length(var.pvt_sub_count) == 0) ? 0 : 0
   )
 }
