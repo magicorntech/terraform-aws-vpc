e4ad41132ddd37e0d95a55110228d07cfc325e6a
fevzitopcu <fevzitopcu@gmail.com>
2024-05-22 17:15:50 +0300

    vpc flow log added

---
+++ flow_log.tf
@@ -0,0 +1,137 @@
+#Create flow log with CloudWatch Log Group as target
+resource "aws_flow_log" "main_cloudwatch" {
+  count                    = var.vpc_flow_log == true ? 1 : 0
+  vpc_id                   = aws_vpc.main.id
+  iam_role_arn             = aws_iam_role.main_cloudwatch[0].arn
+  log_destination          = aws_cloudwatch_log_group.main[0].arn
+  traffic_type             = "ALL"
+  max_aggregation_interval = 60
+  log_format               = "$${account-id} $${action} $${az-id} $${bytes} $${dstaddr} $${dstport} $${end} $${flow-direction} $${instance-id} $${interface-id} $${log-status} $${packets} $${pkt-dst-aws-service} $${pkt-dstaddr} $${pkt-src-aws-service} $${pkt-srcaddr} $${protocol} $${region} $${srcaddr} $${srcport} $${start} $${sublocation-id} $${sublocation-type} $${subnet-id} $${tcp-flags} $${traffic-path} $${type} $${version} $${vpc-id}"
+
+  tags = {
+    Name        = "${var.tenant}-${var.name}-${aws_vpc.main.id}-flow-logs-cw-${var.environment}"
+    Tenant      = var.tenant
+    Project     = var.name
+    Environment = var.environment
+    Maintainer  = "Magicorn"
+    Terraform   = "yes"
+  }
+}
+
+# Create Cloudwatch Log Group to collect VPC flow logs
+resource "aws_cloudwatch_log_group" "main" {
+  count             = var.vpc_flow_log == true ? 1 : 0
+  name              = "${var.tenant}-${var.name}-${aws_vpc.main.id}-log-group-${var.environment}"
+  retention_in_days = 7
+
+  tags = {
+    Name        = "${var.tenant}-${var.name}-${aws_vpc.main.id}-log-group-${var.environment}"
+    Tenant      = var.tenant
+    Project     = var.name
+    Environment = var.environment
+    Maintainer  = "Magicorn"
+    Terraform   = "yes"
+
+  }
+  skip_destroy = true
+}
+
+resource "aws_iam_role" "main_cloudwatch" {
+  count = var.vpc_flow_log == true ? 1 : 0
+  name  = "${var.tenant}-${var.name}-vpc-fl-${aws_vpc.main.id}-cw-role-${var.environment}"
+
+  tags = {
+    Name        = "${var.tenant}-${var.name}-vpc-fl-${aws_vpc.main.id}-cw-role-${var.environment}"
+    Tenant      = var.tenant
+    Project     = var.name
+    Environment = var.environment
+    Maintainer  = "Magicorn"
+    Terraform   = "yes"
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "main_cloudwatch" {
+  count  = var.vpc_flow_log == true ? 1 : 0
+  name   = "${var.tenant}-${var.name}-vpc-fl-${aws_vpc.main.id}-cw-policy-${var.environment}"
+  role   = aws_iam_role.main_cloudwatch[0].id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+# Create S3 Bucker to collect Flow Logs in a S3 Bucket
+resource "random_id" "s3" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "main" {
+  count         = var.vpc_flow_log == true && var.vpc_fl_s3_exp == true ? 1 : 0
+  bucket        = "${var.tenant}-${var.name}-vpc-flow-logs-${random_id.s3.hex}-${var.environment}"
+  force_destroy = false
+
+  tags = {
+    Name        = "${var.tenant}-${var.name}-vpc-flow-logs-${var.environment}"
+    Tenant      = var.tenant
+    Project     = var.name
+    Environment = var.environment
+    Maintainer  = "Magicorn"
+    Terraform   = "yes"
+  }
+}
+
+#Create flow log with s3 as target
+resource "aws_flow_log" "main_s3" {
+  count                    = var.vpc_flow_log == true && var.vpc_fl_s3_exp == true ? 1 : 0
+  vpc_id                   = aws_vpc.main.id
+  log_destination          = aws_s3_bucket.main[0].arn
+  log_destination_type     = "s3"
+  traffic_type             = "ALL"
+  max_aggregation_interval = 60
+  log_format               = "$${account-id} $${action} $${az-id} $${bytes} $${dstaddr} $${dstport} $${end} $${flow-direction} $${instance-id} $${interface-id} $${log-status} $${packets} $${pkt-dst-aws-service} $${pkt-dstaddr} $${pkt-src-aws-service} $${pkt-srcaddr} $${protocol} $${region} $${srcaddr} $${srcport} $${start} $${sublocation-id} $${sublocation-type} $${subnet-id} $${tcp-flags} $${traffic-path} $${type} $${version} $${vpc-id}"
+
+  destination_options {
+    file_format        = "parquet"
+    per_hour_partition = true
+  }
+
+  tags = {
+    Name        = "${var.tenant}-${var.name}-${aws_vpc.main.id}-flow-logs-s3-${var.environment}"
+    Tenant      = var.tenant
+    Project     = var.name
+    Environment = var.environment
+    Maintainer  = "Magicorn"
+    Terraform   = "yes"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,5 @@ variable "pbl_sub_count" {}
 variable "pvt_sub_count" {}
 variable "eks_sub_count" {}
 variable "db_sub_count" {}
+variable "vpc_flow_log" {}
+variable "vpc_fl_s3_exp" {}
